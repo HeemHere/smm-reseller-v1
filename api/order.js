adb4c9794bfac41e01a35429657e2c3953befb8a
HeemHere <spent18k@gmail.com>
2026-02-15 23:38:32 -0600

    Update PANEL_URL to new SMM panel endpoint

--- api/order.js
+++ api/order.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
   const API_KEY = process.env.API_KEY;
 
   // Replace with your SMM panel API URL
-  const PANEL_URL = "https://yourpanel.com/api/v2";
+  const PANEL_URL = "https://lionfollow.com/api/v2";
 
   const response = await fetch(PANEL_URL, {
     method: "POST",
