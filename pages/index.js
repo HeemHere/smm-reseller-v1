79cbc7866a5c0a9ab4ef773e7f477b36cfe65498
HeemHere <spent18k@gmail.com>
2026-02-16 10:18:15 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,16 @@
 
+export default function Home() {
+  return (
+    <div style={{
+      background: "black",
+      color: "white",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontFamily: "Arial"
+    }}>
+      <h1>DCPYnet Backend Running ✅</h1>
+    </div>
+  );
+}
